f4e034d9921dd9beb22e21273434661cfd326e7c
fhumeres <fhumeres@alum.mit.edu>
2020-08-09 04:36:39 -0400

    fix uid bi-init

--- bi-init.sh
+++ bi-init.sh
@@ -10,8 +10,8 @@ echo "creating certbot dir"
 mkdir ${CERTBOT_PATH}
 echo "changing ownership of certbot dir (for docker execution)"
 sudo chmod -R g+rwx ${CERTBOT_PATH}
-sudo chgrp -R 1000 ${CERTBOT_PATH}
-sudo chown -R 1000 ${CERTBOT_PATH}
+sudo chgrp -R $UID ${CERTBOT_PATH}
+sudo chown -R $UID ${CERTBOT_PATH}
 
 
 #CREATE CERTIFICATES FOR INTERNAL ELASTICSEARCH-KIBANA COMMUNICATION (TSL LAYER)
@@ -19,8 +19,14 @@ sudo chown -R 1000 ${CERTBOT_PATH}
 #docker-compose -f ./certs/create-certs.yml run --rm create_certs
 
 #echo "changing ownership of certificates files (for docker execution)"
-
+echo "creating redis dir: "${HOST_DATA_PATH}"/redis"
+mkdir ${HOST_DATA_PATH}/redis
+echo "creating postgres dir"
+mkdir ${CERTBOT_PATH}/postgres
+echo "creating superset dir"
+mkdir ${CERTBOT_PATH}/superset
+echo "changing ownership of superset dirs (for docker execution)"
 
 sudo chmod -R g+rwx ${HOST_DATA_PATH}
-sudo chgrp -R 1000 ${HOST_DATA_PATH}
-sudo chown -R 1000 ${HOST_DATA_PATH}
+sudo chgrp -R $UID ${HOST_DATA_PATH}
+sudo chown -R $UID ${HOST_DATA_PATH}
