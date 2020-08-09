ede436ca7714155774e458d1d7586c5f9aa74b4d
fhumeres <fhumeres@alum.mit.edu>
2020-08-08 20:06:21 -0400

    ammend

---
+++ superset/create_admin.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash  
+
+set -eu
+
+export FLASK_APP=superset
+
+/usr/local/bin/flask fab create-admin \
+    --username $ADMIN_USERNAME \
+    --firstname $ADMIN_FIRST_NAME \
+    --lastname $ADMIN_LAST_NAME \
+    --email $ADMIN_EMAIL \
+    --password $ADMIN_PWD\
